434ff23cc2836066feb799125881e604613894cd
Jack Legend <92314789+keyofrassilon@users.noreply.github.com>
2022-08-19 08:36:28 -0400

    Add files via upload

--- Scripts/scripts_core/sc_inject.py
+++ Scripts/scripts_core/sc_inject.py
@@ -126,68 +126,14 @@ def sc_prepare_gen_inject(original, self, *args, **kwargs):
     result = original(self, *args, **kwargs)
     return result
 
-#@safe_inject(Timeline, 'simulate')
-def sc_run_timeline_simulate_module(original, self, *args, **kwargs):
-    result = original(self, *args, **kwargs)
-
-    try:
-        if services.current_zone().is_zone_running:
-            is_paused = services.game_clock_service().clock_speed == ClockSpeedMode.PAUSED
-            if not is_paused:
-                ScriptCoreMain.init(self)
-        elif services.current_zone().is_in_build_buy:
-            ScriptCoreMain.on_build_buy_enter_handler(self)
-        else:
-            sc_Vars._running = False
-            sc_Vars._config_loaded = False
-    except BaseException as e:
-        error_trap(e)
-        pass
-
-    return result
-
-#@safe_inject(Zone, 'update')
-def sc_run_zone_update_module(original, self, *args, **kwargs):
-    result = original(self, *args, **kwargs)
-    try:
-        if self.is_zone_running:
-            is_paused = services.game_clock_service().clock_speed == ClockSpeedMode.PAUSED
-            if not is_paused:
-                ScriptCoreMain.init(self)
-        elif self.is_in_build_buy:
-            ScriptCoreMain.on_build_buy_enter_handler(self)
-        else:
-            sc_Vars._running = False
-            sc_Vars._config_loaded = False
-    except BaseException as e:
-        error_trap(e)
-        pass
-
-    return result
-
-#@safe_inject(Zone, 'on_client_connect')
-def sc_thread_on_client_connect_module(original, self, *args, **kwargs):
-    result = original(self, *args, **kwargs)
-
-    try:
-        sc_Event = Event()
-        sc_Event.clear()
-        thread = sc_Watcher(sc_Event)
-        thread.start()
-    except BaseException as e:
-        error_trap(e)
-        pass
-
-    return result
 
 @safe_inject(Zone, 'on_loading_screen_animation_finished')
 def sc_run_zone_load_module(original, self, *args, **kwargs):
     result = original(self, *args, **kwargs)
     try:
-        #ScriptCoreMain.load(self)
         sc_Event = Event()
         sc_Event.clear()
-        thread = sc_Watcher(sc_Event, True)
+        thread = sc_Watcher(sc_Event)
         thread.start()
     except BaseException as e:
         error_trap(e)
