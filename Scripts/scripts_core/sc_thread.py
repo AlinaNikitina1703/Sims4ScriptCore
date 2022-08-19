887147ebd62ab28ecd66c060668ee563d538debd
Jack Legend <92314789+keyofrassilon@users.noreply.github.com>
2022-08-19 08:33:17 -0400

    Add files via upload

--- Scripts/scripts_core/sc_thread.py
+++ Scripts/scripts_core/sc_thread.py
@@ -2,6 +2,7 @@ import time
 from threading import Thread
 
 import services
+from clock import ClockSpeedMode
 
 from scripts_core.sc_jobs import debugger
 from scripts_core.sc_main import ScriptCoreMain
@@ -10,7 +11,7 @@ from scripts_core.sc_script_vars import sc_Vars
 
 class sc_Watcher(Thread):
 
-    def __init__(self, event, once=False, wait=0.0):
+    def __init__(self, event, once=True, wait=0.0):
         Thread.__init__(self)
         self.sc_core = ScriptCoreMain()
         self.stopped = event
@@ -26,10 +27,19 @@ class sc_Watcher(Thread):
             else:
                 time.sleep(sc_Vars.update_speed)
                 current_zone = services.current_zone()
+                is_paused = services.game_clock_service().clock_speed == ClockSpeedMode.PAUSED
                 if current_zone is not None:
-                    if current_zone.is_zone_running:
+                    if current_zone.is_zone_running and not is_paused:
                         self.sc_core.init()
-                    elif current_zone.is_zone_shutting_down:
+                    elif current_zone.is_zone_running:
+                        continue
+                    elif current_zone.is_in_build_buy:
+                        self.sc_core.on_build_buy_enter_handler()
+                    else:
+                        sc_Vars._running = False
+                        sc_Vars._config_loaded = False
+
+                    if current_zone.is_zone_shutting_down:
                         self.stopped.set()
                         debugger("Thread stopped!")
                         break
