51cfb1e12e5ac72d173969edb8a64c89c6ecbef9
Jack Legend <92314789+keyofrassilon@users.noreply.github.com>
2022-08-20 12:14:12 -0400

    Add files via upload

--- Scripts/scripts_core/sc_main.py
+++ Scripts/scripts_core/sc_main.py
@@ -247,10 +247,10 @@ class ScriptCoreMain:
                 object_manager = services.object_manager()
                 doors = [obj for obj in services.object_manager().valid_objects() if obj.has_component(PORTAL_COMPONENT)]
                 for sim in services.sim_info_manager().instanced_sims_gen():
+                    if client.active_sim.sim_info.is_in_travel_group():
+                        travel_group = get_sim_travel_group(client.active_sim, False)
+                        sim.sim_info.assign_to_travel_group(travel_group)
                     if sim.sim_info.is_selectable:
-                        if client.active_sim.sim_info.is_in_travel_group():
-                            travel_group = get_sim_travel_group(client.active_sim, False)
-                            sim.sim_info.assign_to_travel_group(travel_group)
                         make_sim_at_work(sim.sim_info)
                         activate_sim_icon(sim.sim_info)
 
